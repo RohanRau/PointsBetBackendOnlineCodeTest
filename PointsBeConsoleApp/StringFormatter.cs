cbcb5b3bb9fe75fdfe69fa56695f2873c990c52a
DevRohanRaut <securelzw@gmail.com>
2025-07-27 16:56:30 +1000

    Fixed the issue with typo

--- PointsBeConsoleApp/StringFormatter.cs
+++ PointsBeConsoleApp/StringFormatter.cs
@@ -18,17 +18,17 @@ namespace PointsBetBackendOnlineCodeTest
         //Code to improve
         public static string ToCommaSeparatedList(string[] items, string quote)
         {
-            StringBuilder qry = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
+            StringBuilder query = new StringBuilder(string.Format("{0}{1}{0}", quote, items[0]));
 
             if (items.Length > 1)
             {
                 for (int i = 1; i < items.Length; i++)
                 {
-                    qry.Append(string.Format(", {0}{1}{0}", quote, items[i]));
+                    query.Append(string.Format(", {0}{1}{0}", quote, items[i]));
                 }
             }
 
-            return qry.ToString();
+            return query.ToString();
         }
     }
 }
